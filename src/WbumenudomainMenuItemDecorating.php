b1c4bab15b498bc363620617afbec70a2959b905
stephane888 <kksasteph888@gmail.com>
2021-11-23 05:25:10 +0100

    MAJ

--- src/WbumenudomainMenuItemDecorating.php
+++ src/WbumenudomainMenuItemDecorating.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Drupal\wbumenudomain;
 
 use Drupal\Core\Menu\DefaultMenuLinkTreeManipulators;
@@ -11,112 +12,107 @@ use Drupal\Core\Menu\MenuLinkInterface;
 use Drupal\menu_link_content\Entity\MenuLinkContent;
 use Drupal\Core\Access\AccessResult;
 
-class WbumenudomainMenuItemDecorating extends DefaultMenuLinkTreeManipulators
-{
-
-    private $CurrentHostName;
-
-    private $Wbumenudomain = [];
-
-    /**
-     *
-     * @param AccessManagerInterface $access_manager
-     * @param AccountInterface $account
-     * @param EntityTypeManagerInterface $entity_type_manager
-     */
-    public function __construct(AccessManagerInterface $access_manager, AccountInterface $account, EntityTypeManagerInterface $entity_type_manager)
-    {
-        parent::__construct($access_manager, $account, $entity_type_manager);
-        $this->CurrentHostName = self::getCurrentActiveDomaineByUrl();
-        $this->getCurrentWbumenudomain();
+class WbumenudomainMenuItemDecorating extends DefaultMenuLinkTreeManipulators {
+  private $CurrentHostName;
+  private $Wbumenudomain = [];
+  
+  /**
+   *
+   * @param AccessManagerInterface $access_manager
+   * @param AccountInterface $account
+   * @param EntityTypeManagerInterface $entity_type_manager
+   */
+  public function __construct(AccessManagerInterface $access_manager, AccountInterface $account, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($access_manager, $account, $entity_type_manager);
+    $this->CurrentHostName = self::getCurrentActiveDomaineByUrl();
+    $this->getCurrentWbumenudomain();
+  }
+  
+  /**
+   * Checks access for one menu link instance.
+   *
+   * @param \Drupal\Core\Menu\MenuLinkInterface $instance
+   *        The menu link instance.
+   *        
+   * @return \Drupal\Core\Access\AccessResultInterface The access result.
+   */
+  protected function menuLinkCheckAccess(MenuLinkInterface $link) {
+    $access_result = parent::menuLinkCheckAccess($link);
+    $metadata = $link->getMetaData();
+    $menuName = $link->getMenuName();
+    if ($menuName == 'main' && !empty($metadata['entity_id'])) {
+      
+      // $this->entityTypeManager->getStorage('menu_link_content')->load($metadata['entity_id']);
+      if (!in_array($metadata['entity_id'], $this->Wbumenudomain)) {
+        $access_result = AccessResult::forbidden();
+      }
     }
-
-    /**
-     * Checks access for one menu link instance.
-     *
-     * @param \Drupal\Core\Menu\MenuLinkInterface $instance
-     *            The menu link instance.
-     *
-     * @return \Drupal\Core\Access\AccessResultInterface The access result.
-     */
-    protected function menuLinkCheckAccess(MenuLinkInterface $link)
-    {
-        $access_result = parent::menuLinkCheckAccess($link);
-        $metadata = $link->getMetaData();
-        $menuName = $link->getMenuName();
-        if ($menuName == 'main' && ! empty($metadata['entity_id'])) {
-
-            // $this->entityTypeManager->getStorage('menu_link_content')->load($metadata['entity_id']);
-            if (! in_array($metadata['entity_id'], $this->Wbumenudomain)) {
-                $access_result = AccessResult::forbidden();
-            }
-        }
-        return $access_result->cachePerPermissions();
-    }
-
-    /**
-     *
-     * {@inheritdoc}
-     * @see \Drupal\Core\Menu\DefaultMenuLinkTreeManipulators::checkAccess()
-     */
-    public function checkAccess($tree)
-    {
-        $tree = parent::checkAccess($tree);
-        /**
-         * --
-         */
-        foreach ($tree as $element) {
-            /**
-             *
-             * @var \Drupal\Core\Menu\MenuLinkDefault $link
-             */
-            $link = $element->link;
-            if ($link->getMenuName() == 'main') {
-                // dump($link->getOptions());
-            }
-        }
-        return $tree;
-    }
-
+    return $access_result->cachePerPermissions();
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\Core\Menu\DefaultMenuLinkTreeManipulators::checkAccess()
+   */
+  public function checkAccess($tree) {
+    $tree = parent::checkAccess($tree);
     /**
      * --
      */
-    private function getCurrentWbumenudomain()
-    {
-        $query = \Drupal::entityQuery('wbumenudomain');
-        $query->condition('hostname', $this->CurrentHostName);
-        $ids = $query->execute();
-
-        $Wbumenudomain = Wbumenudomain::loadMultiple($ids);
-        if (! empty($Wbumenudomain)) {
-            /**
-             *
-             * @var \Drupal\wbumenudomain\Entity\Wbumenudomain $Wbumenudomain
-             */
-            $Wbumenudomain = reset($Wbumenudomain);
-            $this->Wbumenudomain = $Wbumenudomain->get('field_element_de_menu_valides')->getValue();
-            if (! empty($this->Wbumenudomain)) {
-                $this->Wbumenudomain = reset($this->Wbumenudomain);
-                $this->Wbumenudomain = Json::decode($this->Wbumenudomain['value']);
-            }
-        } else {
-            \Drupal::messenger()->addWarning("Ce domaine n'est pas encore configurée");
-        }
+    foreach ($tree as $element) {
+      /**
+       *
+       * @var \Drupal\Core\Menu\MenuLinkDefault $link
+       */
+      $link = $element->link;
+      if ($link->getMenuName() == 'main') {
+        // dump($link->getOptions());
+      }
     }
-
-    static public function getCurrentActiveDomaineByUrl()
-    {
-        /**
-         *
-         * @var \Drupal\domain\DomainNegotiator $DomainNegotiator
-         */
-        // $DomainNegotiator = \Drupal::service('domain.negotiator');
-        // $domain = $DomainNegotiator->getActiveId();
-        // if (empty($domain))
-        // \Drupal::messenger()->addWarning("Ce domaine n'est pas encore enregitré, ");
-        // return $domain;
-        $new_value = strtolower($_SERVER['HTTP_HOST']);
-        $new_value = preg_replace('/[^a-z0-9_]+/', '_', $new_value);
-        return preg_replace('/_+/', '_', $new_value);
+    return $tree;
+  }
+  
+  /**
+   * --
+   */
+  private function getCurrentWbumenudomain() {
+    $query = \Drupal::entityQuery('wbumenudomain');
+    $query->condition('hostname', $this->CurrentHostName);
+    $ids = $query->execute();
+    $Wbumenudomain = Wbumenudomain::loadMultiple($ids);
+    if (!empty($Wbumenudomain)) {
+      /**
+       *
+       * @var \Drupal\wbumenudomain\Entity\Wbumenudomain $Wbumenudomain
+       */
+      $Wbumenudomain = reset($Wbumenudomain);
+      $this->Wbumenudomain = $Wbumenudomain->get('field_element_de_menu_valides')->getValue();
+      if (!empty($this->Wbumenudomain)) {
+        $this->Wbumenudomain = reset($this->Wbumenudomain);
+        $this->Wbumenudomain = Json::decode($this->Wbumenudomain['value']);
+      }
     }
+    else {
+      \Drupal::messenger()->addWarning("Ce domaine n'est pas encore configurée");
+    }
+  }
+  
+  static public function getCurrentActiveDomaineByUrl() {
+    /**
+     *
+     * @var \Drupal\domain\DomainNegotiator $DomainNegotiator
+     */
+    $DomainNegotiator = \Drupal::service('domain.negotiator');
+    $domain = $DomainNegotiator->getActiveId();
+    if (empty($domain))
+      \Drupal::messenger()->addWarning("Ce domaine n'est pas encore enregitré");
+    return $domain;
+    // dump($domain);
+    // pourquoi cette zone ?
+    // $new_value = strtolower($_SERVER['HTTP_HOST']);
+    // $new_value = preg_replace('/[^a-z0-9_]+/', '_', $new_value);
+    // return preg_replace('/_+/', '_', $new_value);
+  }
+  
 }
